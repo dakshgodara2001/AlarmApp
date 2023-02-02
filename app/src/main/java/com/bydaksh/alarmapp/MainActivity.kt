1bc910fdf5e4460935a4f43fe6f61ada447a86ce
Daksh Godara <godaradaksh2001@gmail.com>
2023-02-02 11:10:09 +0530

    alarm

---
+++ app/src/main/java/com/bydaksh/alarmapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.bydaksh.alarmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import  kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val saveData=SaveData(applicationContext)
+
+        tvShowTime.text= saveData.getHour().toString() + ":" + saveData.getMinute().toString()
+    }
+
+
+    fun BuSetTime(view:View){
+        val popTime= PopTime()
+        val fm=supportFragmentManager
+        popTime.show(fm,"Select time")
+
+
+    }
+
+    fun SetTime(Hours:Int,Minute:Int){
+
+        tvShowTime.text= "$Hours:$Minute"
+
+        val saveData=SaveData(applicationContext)
+        saveData.SaveData(Hours,Minute)
+        saveData.setAlarm()
+
+
+    }
+}
